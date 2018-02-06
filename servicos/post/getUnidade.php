208f2be6fd931a91a8f5f841cda0737a7f6ee1be
saviossmg <saviossmg@gmail.com>
2018-02-06 12:40:47 -0200

    Alocacao 4.0
    Versão de verificação

---
+++ servicos/post/getUnidade.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Criado por: Sávio Martins Valentim
+ * Data: 21/09/2017
+ * Arquivo responsavel pela listagem de todos os cursos para retornar para o APP
+ */
+
+define('BASE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/');
+require_once BASE_DIR . 'vendor/bootstrap.php';
+include 'getHash.php';
+
+$mensagem;
+$data = [];
+
+try {
+    if(empty($_POST)){
+        $mensagem = "HASH não informado";
+        throw new Exception($mensagem);
+    }
+    else
+    if(sha1($_POST["hash"]) != $hash) {
+        $mensagem = "HASH incorreto!";
+        throw new Exception($mensagem);
+    }
+    else{
+        $qb = $entityManager->createQueryBuilder();
+        $qb->select("c")
+            ->from('Unidade', "c")
+            ->andWhere("c.id IS NOT NULL ");
+
+        $rs = $qb->getQuery()->getResult();
+        //contador de registros
+        $qCount = clone $qb;
+        $qCount->select("count(c.id)");
+        $totalregistro = $qCount->getQuery()->getSingleScalarResult();
+
+        if ($totalregistro > 0) {
+            foreach ($rs as $idx => $model) {
+                $data[$idx]["id"] = $model->getId();
+                $data[$idx]["nome"] = $model->getNome();
+                $data[$idx]["endereco"] = $model->getEndereco();
+                $data[$idx]["cep"] = $model->getCep();
+                $data[$idx]["latitude"] = $model->getLatitude();
+                $data[$idx]["longitude"] = $model->getLongitude();
+            }
+            $mensagem =  $totalregistro." registros encontrados";
+            $resultado = ['status' => true, 'mensagem' => $mensagem, 'data' => $data];
+        } else {
+            // records now found
+            $mensagem = "Nenhum registro foi encontrado.";
+            $resultado = ['status' => false, 'mensagem' => $mensagem, 'data' => null];
+        }
+    }
+
+} catch (Exception $ex) {
+    $mensagem = "Atenção: ".$ex->getMessage();
+    $resultado = ['status' => false, 'mensagem' => $mensagem, 'data' => null];
+}
+$retorno = json_encode($resultado);
+
+echo $retorno;
+?>
